2a2386d6fbb7000755260def78c3e4dce899ced3
Zack Ankner <zackankner@gmail.com>
2020-02-28 22:10:07 -0500

    feat(master) Input pipeline finished except for structural data changes

--- preprocess/process.py
+++ preprocess/process.py
@@ -1,6 +1,8 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+import os
 
 class Process(object):
 
@@ -10,9 +12,66 @@ class Process(object):
 
   def get_datasets(self):
     dataset, info = tfds.load('multi_nli:1.0.0', with_info=True)
-    train = dataset['train']
-    test = dataset['validation_matched']
-    return train_dataset, test_dataset
+    train_dataset = dataset['train']
+    val_dataset = dataset['validation_matched']
+    train_dataset = train_dataset.map(self._unpack_vars)
+    val_dataset = val_dataset.map(self._unpack_vars)
+    self._build_encoders(train_dataset, val_dataset)
+    train_dataset = train_dataset.map(
+        lambda inp, val: self._py_encode(inp, val, True))
+    val_dataset = val_dataset.map(
+        lambda inp, val: self._py_encode(inp, val, False))
+    return train_dataset, val_dataset
+
+  def _build_encoders(self, train, val):
+    train_tokenizer = None
+    if (os.path.exists('tokenizers/train_tokenizer.subwords')):
+      train_tokenizer = tfds.features.text.SubwordTextEncoder \
+        .load_from_file('tokenizers/train_tokenizer')
+    else:
+      train_tokenizer = tfds.features.text.SubwordTextEncoder \
+        .build_from_corpus(
+            (inp.numpy() for inp, label in train), 
+            2**13)
+      train_tokenizer.save_to_file('tokenizers/train_tokenizer')
+  
+    test_tokenizer = None
+    if (os.path.exists('tokenizers/test_tokenizer.subwords')):
+      test_tokenizer = tfds.features.text.SubwordTextEncoder \
+        .load_from_file('tokenizers/test_tokenizer')
+    else:
+      test_tokenizer = tfds.features.text.SubwordTextEncoder \
+        .build_from_corpus(
+            (label.numpy() for inp, label in train), 2**13)
+      test_tokenizer.save_to_file('tokenizers/test_tokenizer')
+
+    self.train_tokenizer = train_tokenizer
+    self.test_tokenizer = test_tokenizer
+
+  def _unpack_vars(self, dataset):
+    text = dataset['premise']
+    label = dataset['hypothesis']
+
+    return text, label
+
+  def _py_encode(self, text, label, train):
+    text, label = tf.py_function(
+        self._encode, [text, label, train], [tf.int64, tf.int64])
+    return text, label
+
+  def _encode(self, text, label, train):
+    tokenizer = None
+    if train:
+      tokenizer = self.train_tokenizer
+    else:
+      tokenizer = self.test_tokenizer
+    text = [tokenizer.vocab_size] + tokenizer.encode(
+        text.numpy()) + [tokenizer.vocab_size + 1]
+
+    label = [tokenizer.vocab_size] + tokenizer.encode(
+        label.numpy()) + [tokenizer.vocab_size + 1]
+
+    return text, label
 
   def _build_dataset(self, data_dir, train_data):
     data_files = self._load_files(data_dir)
@@ -59,3 +118,12 @@ class Process(object):
   
 #  def _test(self):
     # Add code to display or test data in some way
+
+p = Process(10,1)
+train, val = p.get_datasets()
+for sent, hyp in train.take(1):
+  print(sent.numpy())
+  print(hyp.numpy())
+for sent, hyp in val.take(1):
+  print(sent.numpy())
+  print(hyp.numpy())
