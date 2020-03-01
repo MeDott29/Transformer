cf075fe9663b8fbbc359ce03ca15ab0ec8ca1e68
Zack Ankner <zackankner@gmail.com>
2020-02-29 23:22:54 -0500

    feat(master) temp state while working out padding shape

--- preprocess/process.py
+++ preprocess/process.py
@@ -9,6 +9,7 @@ class Process(object):
   def __init__(self, batch_size, pre_fetch):
     self.batch_size = batch_size
     self.pre_fetch = pre_fetch
+    self.train_len = 392702
 
   def get_datasets(self):
     dataset, info = tfds.load('multi_nli:1.0.0', with_info=True)
@@ -21,6 +22,8 @@ class Process(object):
         lambda inp, val: self._py_encode(inp, val, True))
     val_dataset = val_dataset.map(
         lambda inp, val: self._py_encode(inp, val, False))
+    train_dataset = self._prepare(train_dataset, True)
+    test_dataset = self._prepare(test_dataset, False)
     return train_dataset, val_dataset
 
   def _build_encoders(self, train, val):
@@ -51,7 +54,6 @@ class Process(object):
   def _unpack_vars(self, dataset):
     text = dataset['premise']
     label = dataset['hypothesis']
-
     return text, label
 
   def _py_encode(self, text, label, train):
@@ -73,51 +75,14 @@ class Process(object):
 
     return text, label
 
-  def _build_dataset(self, data_dir, train_data):
-    data_files = self._load_files(data_dir)
-    dataset = self._load_dataset(data_files)
-
-    if train_data:
-      dataset = self._normalize_data(dataset)
-      dataset = self._augment_data(dataset)
-
-    dataset = self._prepare(dataset)
-    return dataset 
-
   def _prepare(self, dataset, train):
     if train:
-      dataset = dataset.cache().shuffle(data_len).batch(self.batch_size)
-      dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+      dataset = dataset.cache().shuffle(
+          self.train_len).padded_batch(self.batch_size, [None])
+      dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     else:
-      dataset = dataset.batch(self.batch_size)
+      dataset = dataset.padded_batch(self.batch_size, [None])
     return dataset  
-
- # def _normalize(self, input_, label):
- #   # Normalize input data: 
-
- #   return input_, label
- # 
- # def _augment_data(self, input_, label):
- #   # Augment training data here
-
- #   return input_, label
-  
-  @tf.function
-  def _load_dataset(self, data_files):
-    dataset = tf.data.Dataset.from_tensor_slices(data_files)
-    dataset = dataset.map(
-      self._load_datapoint, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
-    return dataset 
-  
-  @tf.function
-  def _load_datapoint(self, input_file, label_file):
-    # Add code to load input and label file:
-
-    return input_, label
-  
-#  def _test(self):
-    # Add code to display or test data in some way
 
 p = Process(10,1)
 train, val = p.get_datasets()
